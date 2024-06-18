87cb1cca94a30fb290dcaf6fe8099979108074b4
J.Manuel <j.manuelmohedano@live.com.mx>
2024-06-17 23:18:39 -0600

    Proyeto de clase

---
+++ src/com/aluracursos/screenmatch/principal/Principal.java
@@ -0,0 +1,64 @@
+package com.aluracursos.screenmatch.principal;
+
+import com.aluracursos.screenmatch.calculos.CalculadoraDeTiempo;
+import com.aluracursos.screenmatch.calculos.FiltroRecomendacion;
+import com.aluracursos.screenmatch.modelos.Episodio;
+import com.aluracursos.screenmatch.modelos.Pelicula;
+import com.aluracursos.screenmatch.modelos.Serie;
+
+import java.util.ArrayList;
+
+public class Principal {
+//    public static void main(String[] args) {
+//        Pelicula pelicula = new Pelicula("Encanto",2021);
+//        pelicula.setDuracionEnMinutos(120);
+//        pelicula.setIncluidoEnElPlan(true);
+//        pelicula.evalua(9.9);
+//        pelicula.evalua(5);
+//        pelicula.evalua(10.0);
+//        pelicula.muestraFichaTecnica();
+//
+//        if(pelicula.isIncluidoEnElPlan()) {
+//            System.out.println(pelicula.getTotalDeLasEvaluaciones());
+//            System.out.println(pelicula.getSumaDeLasEvaluaciones());
+//            System.out.println(pelicula.calculaMedia());
+//        }else{
+//            System.out.println("La pelicula no está incluida en el plan contratado");
+//        }
+//
+//        Serie casaDragon = new Serie("La casa del dragon",2022);
+//        casaDragon.setTemporadas(1);
+//        casaDragon.setMinutosPorEpisodio(50);
+//        casaDragon.setEpisodiosPorTemporada(10);
+//        casaDragon.muestraFichaTecnica();
+//        System.out.println(casaDragon.getDuracionEnMinutos());
+//
+//        CalculadoraDeTiempo calculadora = new CalculadoraDeTiempo();
+//        calculadora.incluye(pelicula);
+//        calculadora.incluye(casaDragon);
+//        System.out.println(calculadora.getTiempoTotal());
+//
+//        FiltroRecomendacion filtroRecomendacion = new FiltroRecomendacion();
+//        filtroRecomendacion.filtra(pelicula);
+//
+//        Episodio episodio = new Episodio();
+//        episodio.setNumero(1);
+//        episodio.setNombre("algo de got");
+//        episodio.setSerie(casaDragon);
+//        episodio.setTotalVisualizaciones(50);
+//        filtroRecomendacion.filtra(episodio);
+//
+//        var peliculaDeBruno = new Pelicula("El señor de los anillos",2021);
+//        peliculaDeBruno.setDuracionEnMinutos(180);
+//
+//        ArrayList<Pelicula> listaDePeliculas = new ArrayList<>();
+//
+//        listaDePeliculas.add(pelicula);
+//        listaDePeliculas.add(peliculaDeBruno);
+//
+//        System.out.println("Tamaño de la lista "+listaDePeliculas.size());
+//        System.out.println("La primera pelicula es "+listaDePeliculas.get(0).getNombre());
+//        System.out.println(listaDePeliculas);
+//        System.out.println("toString de la pelicula = "+listaDePeliculas.get(0).toString());
+//    }
+}
